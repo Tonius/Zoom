e170ac8f0d01340d872bca867d51845a336eef24
Tonius <tonbro96@gmail.com>
2015-10-08 17:22:00 +0200

    Fixed some zoom overlay rendering bugs

--- src/main/java/tonius/zoom/client/EventHandler.java
+++ src/main/java/tonius/zoom/client/EventHandler.java
@@ -2,6 +2,7 @@ package tonius.zoom.client;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
@@ -11,6 +12,9 @@ import net.minecraftforge.client.event.MouseEvent;
 import net.minecraftforge.client.event.RenderHandEvent;
 import net.minecraftforge.client.event.RenderPlayerEvent;
 import net.minecraftforge.common.MinecraftForge;
+
+import org.lwjgl.opengl.GL11;
+
 import tonius.zoom.ItemBinoculars;
 import tonius.zoom.Zoom;
 import cpw.mods.fml.common.FMLCommonHandler;
@@ -22,10 +26,12 @@ import cpw.mods.fml.common.gameevent.TickEvent.RenderTickEvent;
 public class EventHandler {
     
     private static final Minecraft mc = Minecraft.getMinecraft();
+    private static final ResourceLocation OVERLAY_TEXTURE = new ResourceLocation(Zoom.MODID, "textures/gui/binoculars.png");
+    
     private static final float MIN_ZOOM = 1 / 1.5F;
     private static final float MAX_ZOOM = 1 / 10.0F;
-    
     private static float currentZoom = 1 / 6.0F;
+    
     private static boolean renderPlayerAPILoaded = false;
     
     public static void init() {
@@ -58,12 +64,19 @@ public class EventHandler {
         }
         
         if (isUsingBinoculars() && mc.gameSettings.thirdPersonView == 0) {
+            GL11.glPushMatrix();
             mc.entityRenderer.setupOverlayRendering();
+            GL11.glEnable(GL11.GL_BLEND);
+            OpenGlHelper.glBlendFunc(770, 771, 1, 0);
+            GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+            GL11.glDisable(GL11.GL_ALPHA_TEST);
+            
+            mc.renderEngine.bindTexture(OVERLAY_TEXTURE);
+            
             ScaledResolution res = new ScaledResolution(mc, mc.displayWidth, mc.displayHeight);
             double width = res.getScaledWidth_double();
             double height = res.getScaledHeight_double();
             
-            mc.renderEngine.bindTexture(new ResourceLocation(Zoom.MODID, "textures/gui/binoculars.png"));
             Tessellator tessellator = Tessellator.instance;
             tessellator.startDrawingQuads();
             tessellator.addVertexWithUV(0.0D, height, -90.0D, 0.0D, 1.0D);
@@ -71,6 +84,8 @@ public class EventHandler {
             tessellator.addVertexWithUV(width, 0.0D, -90.0D, 1.0D, 0.0D);
             tessellator.addVertexWithUV(0.0D, 0.0D, -90.0D, 0.0D, 0.0D);
             tessellator.draw();
+            
+            GL11.glPopMatrix();
         }
     }
     
